190ef0d0bb6d13b481293f9db311d55270f39165
Oleg <oleg.chorny@gmail.com>
2018-12-14 23:51:11 +0200

    first commit

---
+++ pkg/apis/fortio/v1alpha1/loadtest_types.go
@@ -0,0 +1,45 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// LoadTestSpec defines the desired state of LoadTest
+type LoadTestSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+}
+
+// LoadTestStatus defines the observed state of LoadTest
+type LoadTestStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// LoadTest is the Schema for the loadtests API
+// +k8s:openapi-gen=true
+type LoadTest struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   LoadTestSpec   `json:"spec,omitempty"`
+	Status LoadTestStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// LoadTestList contains a list of LoadTest
+type LoadTestList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []LoadTest `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&LoadTest{}, &LoadTestList{})
+}
